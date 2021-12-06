3bd86909276667a28ccf2d77f49df86f0573a04f
Fombi Favour <fombifavour@gmail.com>
2021-12-06 02:27:19 +0100

    intro commit

---
+++ testing/testing/testing/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace testing
+{
+    class MainActivity : ContentPage
+    {
+    }
+}
