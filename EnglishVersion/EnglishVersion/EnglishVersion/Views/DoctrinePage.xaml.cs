eda3bf58ddfc8490eac9724df75c6c8eef5008bf
Fombi Favour <fombifavour@gmail.com>
2021-12-06 02:29:22 +0100

    English version

---
+++ EnglishVersion/EnglishVersion/EnglishVersion/Views/DoctrinePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EnglishVersion.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DoctrinePage : ContentPage
+    {
+        public DoctrinePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
